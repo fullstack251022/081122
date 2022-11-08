75b78edbfc5688cd8d7503d0828b9d7601ee9b57
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-11-08 21:20:57 +0200

    Create logicalOperators.java

---
+++ logicalOperators.java
@@ -0,0 +1,112 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        int x = 4;
+
+//        if (x > 5 && (x % 2 == 0)) {  //&& AND  (both should be true)
+//            System.out.println("The number is higher then 5 and the number is even");
+//        }
+
+//        if(x > 5 || x % 2 == 0){ //|| if  (only one condition should be true)
+//            System.out.println("The number is higher then 5 OR the number is even");
+//        }
+
+//        int x =10;
+
+//        if( true && true && true && false){
+//            System.out.println("I am here");
+//        }
+
+//        if( true || true && true && false){
+//            System.out.println("I am here");
+//        }
+
+//        if( true || (true || true && true) && false){
+//            System.out.println("I am here");
+//        }
+
+//        int age = 12;
+//        boolean isHasDiploma =false;
+
+        //age (int), hasDiploma (boolean), int nekudotNehut
+
+
+        // print ("You allowed to start work in this place") if user hasDiploma and his age higher than 18 using one if statement
+        // if user has more than 10 nekudotNehut also let him enter to the room
+
+//        int age = 15;
+//        boolean hasDiploma = false;
+//        int nekudotNehut = 20;
+////
+//        if( age > 18 && hasDiploma || nekudotNehut>10){
+//            System.out.println("You allowed to start work in this place");
+//        }
+
+
+//        if (!(false) ){
+//            System.out.println("I am here ");
+//        }
+
+//        int x = 2;
+//
+//        switch(x) {
+//            case 1:
+//                System.out.println("The number is one ");
+//                break;
+//            case 2:
+//                System.out.println("The number is two ");
+//                break;
+//            case 3:
+//                System.out.println("The number is three ");
+//                break;
+//            default:
+//                System.out.println("The number is not one not two and not three");
+//        }
+
+
+
+
+//        int day = 4;
+//        switch (day) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//        }
+
+
+//        variable = (condition) ? expressionTrue :  expressionFalse;
+
+//        int age =16;
+//        String isAdult;
+//
+//        if (age>18){
+//            isAdult="I am adult";
+//        }else {
+//            isAdult ="I am young";
+//        }
+
+//        int age =20;
+//        String isAdult = (age>18) ? "I am adult" :  "I am young";
+//        System.out.println(isAdult);
+
+
+    }
+}
